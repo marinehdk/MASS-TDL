f7eec50fbe0e987b5db5cdf9d0a1f5ef53b1dec5
marinehdk <2797197392@qq.com>
2026-05-20 11:24:17 +0800

    feat(d1.3.1): ShipMotionSimulator ABC + FmuDescriptor (T1)

---
+++ src/sim_workbench/sil_nodes/ship_dynamics/ship_dynamics/__init__.py
@@ -0,0 +1,7 @@
+from .mmg_model import ShipState, MMGModel
+from .mmg_coefficients import MMGCoefficients
+from .ship_motion_simulator import (
+    ShipMotionSimulator,
+    FmuDescriptor,
+    FmuVariableSpec,
+)
