1d9eac6be624d36ecc2245434370f9ac6432b65d
marinehdk <2797197392@qq.com>
2026-06-22 17:43:32 +0800

    fix local runtime default profile

--- scripts/local-a4000-env.sh
+++ scripts/local-a4000-env.sh
@@ -10,14 +10,14 @@ if [[ ! -f "${_mass_l3_tile_dir}/trondelag.mbtiles" && "${_mass_l3_repo_root}" =
 fi
 
 export COMPOSE_FILE=docker-compose.yml:docker-compose.a4000.yml:docker-compose.plugins.yml
-export COMPOSE_PROFILES="${COMPOSE_PROFILES:-plugins}"
+export COMPOSE_PROFILES="${COMPOSE_PROFILES:-}"
 export ROS_DOMAIN_ID=42
 export ORCH_PORT="${ORCH_PORT:-18000}"
 export FOX_PORT="${FOX_PORT:-18765}"
 export MASS_L3_TILE_DIR="${MASS_L3_TILE_DIR:-${_mass_l3_tile_dir}}"
 export ORCH_URL="${ORCH_URL:-https://127.0.0.1:18000}"
-export TDL_INTEGRATION_PROFILE="${TDL_INTEGRATION_PROFILE:-a4000_external}"
-export TDL_RUNTIME_PROFILE="${TDL_RUNTIME_PROFILE:-integration-local}"
+export TDL_INTEGRATION_PROFILE="${TDL_INTEGRATION_PROFILE:-default}"
+export TDL_RUNTIME_PROFILE="${TDL_RUNTIME_PROFILE:-internal-local}"
 export SIL_ORCHESTRATOR_CPUS="${SIL_ORCHESTRATOR_CPUS:-1.0}"
 export SIL_NODES_CPUS="${SIL_NODES_CPUS:-4.0}"
 export FOXGLOVE_CPUS="${FOXGLOVE_CPUS:-1.0}"
